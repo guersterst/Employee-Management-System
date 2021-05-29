9dbf5109ceccaf519c5cc9b93a61127860ab5aa8
Elias Goller <goller@fim.uni-passau.de>
2021-05-29 09:44:23 +0200

    Quick fix to allow us to test the front-end. Will be replaced later.

--- src/main/java/group9/employee_management/EmployeeManagementApplication.java
+++ src/main/java/group9/employee_management/EmployeeManagementApplication.java
@@ -2,8 +2,9 @@ package group9.employee_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // Quick fix to get things running non the front-end side
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
