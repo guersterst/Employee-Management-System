aca9c9ff9281fdb420b2c41f4a5633fc4f5bfb39
Elias Goller <goller@fim.uni-passau.de>
2021-05-29 10:33:14 +0200

    Removed comment.

--- src/main/java/group9/employee_management/TestController.java
+++ src/main/java/group9/employee_management/TestController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
-    @GetMapping("") //HTTP GET requests with /greeting in URL are mapped to this method
-    //Parameter name is bound into name var of greeting. However, name is not 'required' --> defaultValue is used instead
+    @GetMapping("")
     public String index() {
         return "index";
     }
