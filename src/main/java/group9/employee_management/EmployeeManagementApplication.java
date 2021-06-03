3770f8f33892bccc6ad7b56e505a59ce6c46ea23
guersterst <guerster@fim.uni-passau.de>
2021-06-03 11:23:15 +0200

    Fix in order to run front-end

--- src/main/java/group9/employee_management/EmployeeManagementApplication.java
+++ src/main/java/group9/employee_management/EmployeeManagementApplication.java
@@ -3,8 +3,9 @@ package group9.employee_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // Quick fix to get things running non the front-end side
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, ErrorMvcAutoConfiguration.class }) // Quick fix to get things running non the front-end side
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
