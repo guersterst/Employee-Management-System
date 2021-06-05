29bff740d11976622691d193b853c8153dc5a52c
Elias Goller <goller@fim.uni-passau.de>
2021-06-05 19:01:45 +0200

    Added functionality to make a POST-Request to login. For this, Thymeleaf-functionality was added to index.html. The TestController received a method to handle the request and loginFormSubmission is used as a model. Please note: Both Java-classes are for testing-purposes only to test whether the request works. No actual validation yet.

--- src/main/java/group9/employee_management/TestController.java
+++ src/main/java/group9/employee_management/TestController.java
@@ -3,12 +3,22 @@ package group9.employee_management;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        System.out.println("Hallo");
+        model.addAttribute("loginForm", new loginFormSubmission());
         return "index";
+    }
+
+    @PostMapping("")
+    public String submissionResult(@ModelAttribute("loginForm") loginFormSubmission submission) {
+        System.out.println("Hallo");
+        return "employeeView";
     }
 }
---
+++ src/main/java/group9/employee_management/loginFormSubmission.java
@@ -0,0 +1,20 @@
+package group9.employee_management;
+
+public class loginFormSubmission {
+    private String userName;
+    private String password;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
