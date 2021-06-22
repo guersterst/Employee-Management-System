665b250d639e31f0d9ad2f85dc8e6531f58313c2
johannes-garstenauer <64467583+johannes-garstenauer@users.noreply.github.com>
2021-06-22 15:31:35 +0200

    Added WorkSessionRepository.

---
+++ src/main/java/group9/employee_management/persistence/repositories/WorkSessionRepository.java
@@ -0,0 +1,10 @@
+package group9.employee_management.persistence.repositories;
+
+import group9.employee_management.persistence.entities.WorkSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkSessionRepository extends JpaRepository<WorkSession, Integer> {
+}
