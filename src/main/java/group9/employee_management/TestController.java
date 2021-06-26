72a9d7ced265dd2f6fb5aacc4049a0ae552e873b
guersterst <guerster@fim.uni-passau.de>
2021-06-26 21:43:28 +0200

    employeeList data from server

--- src/main/java/group9/employee_management/TestController.java
+++ src/main/java/group9/employee_management/TestController.java
@@ -5,10 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+/*@Controller
 public class TestController {
     @GetMapping("")
     public String index() {
         return "adminView";
     }
 }
+
+
+ */
