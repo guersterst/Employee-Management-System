dfc2972541f2d682acba48aab6b047f5011812b0
guersterst <guerster@fim.uni-passau.de>
2021-06-30 15:06:23 +0200

    Removed test prints from controller.

--- src/main/java/group9/employee_management/web/controller/EmployeeWorkSessionsHistoryController.java
+++ src/main/java/group9/employee_management/web/controller/EmployeeWorkSessionsHistoryController.java
@@ -192,9 +192,7 @@ public class EmployeeWorkSessionsHistoryController {
     @ResponseBody
     public ResponseEntity<byte[]> getSessionsJSON(Principal principal,
                                                   @ModelAttribute("status") StatusDTO status) {
-        System.out.println("Is in");
         byte[] customerJsonBytes;
-        System.out.println("Called");
         try {
             customerJsonBytes = workSessionService.workSessionsToJSON(principal.getName()).getBytes();
         } catch (NoSessionsException | NoSuchUserException exception) {
