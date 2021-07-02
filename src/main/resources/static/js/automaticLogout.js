7d94e7d2883b079043a8744886cc0dfb624cda9c
Elias Goller <goller@fim.uni-passau.de>
2021-07-02 16:58:27 +0200

    Added extensive comments to automaticLogout.js.

--- src/main/resources/static/js/automaticLogout.js
+++ src/main/resources/static/js/automaticLogout.js
@@ -4,12 +4,18 @@
 // When the page is loaded, we start a timer
 // Loading a page indicates activity, so it's fair to start a timer.
 
-let standardCounterTime = 1000 * 5;//115;
-let counter = standardCounterTime;
-let interval;
-let text = $('#timer');
-let lastFiveSecondsCounter = 5;
+// After 115 seconds, we call startLogOutTimer(), which displays
+// a countdown for the last five seconds. startLogOutTimer() then
+// sets up a new timer and calls itself again after one second.
+// This happens until the timer reaches zero. Then, the actual
+// logout is performed by calling logout().
+let standardCounterTime = 1000 * 115;
+let counter = standardCounterTime; // (re)set the counter to its default value. Stores the milliseconds.
+let interval; // Here, we store the interval so we can stop it in case the user performs an action.
+let text = $('#timer'); // Used update the timer's text.
+let lastFiveSecondsCounter = 5; // Keeps track of how often we need to call startLogOutTimer(). != milliseconds!
 
+// When the page is loaded, we start the timer
 window.onload = function() {
     counter = standardCounterTime;
     interval = setInterval(startLogOutTimer, counter)
@@ -50,6 +56,9 @@ window.onmouseup = function() {
     resetTimer()
 }
 
+// Here, we reset the timer if the user has performed an action and thus shows that they are active.
+// For this, we need to restore the initial values, stop the current timer (clearInterval) and start a
+// new one.
 function resetTimer() {
     lastFiveSecondsCounter = 5
     counter = standardCounterTime
@@ -58,6 +67,15 @@ function resetTimer() {
     interval = setInterval(startLogOutTimer, counter)
 }
 
+// If 115 seconds have passed, startLogOoutTimer is called for the first time.
+// In case lastFiveSecondsCounter, which stores how often we have called startLogOutTimer(),
+// is greater than zero, we update the timer's text, clear the current interval,
+// reduce the number of times we still need to call startLogOutTimer() and set a new
+// interval. This ensures that after another second has passed, startLogOutTimer() is called again.
+// This happens until lastFiveSecondsCounter reaches zero. Then, we know that we need to perform a logout.
+// We set a new timer and log the user out after one second, which is done by calling logout().
+// Note: If the user is on a page containing "/login" ("/login", "/login?error", "/login?logout"), no
+// logout should be performed.
 function startLogOutTimer() {
     if (!(containsSubstring(document.URL, "/login"))) {
         if (lastFiveSecondsCounter > 0) {
@@ -73,6 +91,7 @@ function startLogOutTimer() {
     }
 }
 
+// Logout the user.
 function logout() {
     lastFiveSecondsCounter = 5
     counter = standardCounterTime
@@ -83,11 +102,16 @@ function logout() {
 }
 
 
+// Checks, whether substring is found within mainString.
+// Used to check whether the URL which the user is currently on contains "login".
+// If it does, there is no need to perform a logout as the user hasn't even been logged in.
+// Logging the user out on the login page would lead to the logout being performed every
+// two minutes even if nobody uses the terminal.
+
 function containsSubstring(mainString, substring) {
     let substringCharPos = 0
 
     for (let i = 0; i < mainString.length; i++) {
-        console.log(i);
         if (mainString.charAt(i) === substring.charAt(substringCharPos)) {
             substringCharPos++;
             if (substringCharPos >= substring.length) {
@@ -98,6 +122,5 @@ function containsSubstring(mainString, substring) {
         }
     }
 
-    console.log(mainString + " contains " + substring + ": " + (substringCharPos === substring.length)  + ", " + substringCharPos);
     return substringCharPos === substring.length;
 }
