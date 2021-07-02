08ec1421f69d814753e7c0eeaa44831595515155
Elias Goller <goller@fim.uni-passau.de>
2021-07-02 13:37:36 +0200

    Added a very simple errorPage.

---
+++ src/main/java/group9/employee_management/web/controller/ErrorPageController.java
@@ -0,0 +1,24 @@
+package group9.employee_management.web.controller;
+
+import group9.employee_management.web.dto.StatusDTO;
+import group9.employee_management.web.dto.UserDTO;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorPageController implements ErrorController {
+    /**
+     * Mapping for a simple error page.
+     * @return The errorPage.
+     */
+    @GetMapping(
+            value = ""
+    )
+    public String get() {
+        return "errorPage";
+    }
+}
