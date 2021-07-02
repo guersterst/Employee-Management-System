9c534a20ddbbdfe52038701b1b31cef7248ef23d
Elias Goller <goller@fim.uni-passau.de>
2021-07-02 13:44:42 +0200

    Nicer looking errorPage, also added menubar and footer.

--- src/main/java/group9/employee_management/web/controller/ErrorPageController.java
+++ src/main/java/group9/employee_management/web/controller/ErrorPageController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller that's responsible for redirecting to our error page.
+ */
 @Controller
 @RequestMapping("/error")
 public class ErrorPageController implements ErrorController {
